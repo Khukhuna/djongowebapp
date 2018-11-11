7e93a606522807cf056a13888969e5b29a291310
FiNkacho <sabaxuxunashvili@gmail.com>
2018-11-11 13:54:33 +0400

    get data from back & display

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Message
 
 # Create your views here.
+
+def message(req):
+    messages = Message.objects.all()
+    return render(req, 'main/message.html', {"messages": messages})
